ebca876ff1fd45623afacef09f6e42cd87a2d438
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2023-11-25 23:49:39 -0600

    Se agregan estilo a tablas y se crea la sección features

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const left = document.getElementById('arrowLeft');
 const right = document.getElementById('arrowRight');
-const tableCurrency =document.getElementById('tableCurrency');
-const tableCommissions =document.getElementById('tableCommissions');
+const tableCurrency = document.getElementById('tableCurrency');
+const tableCommissions = document.getElementById('tableCommissions');
+const showDate = document.getElementById('changeColor');
+
 
 left.addEventListener("click", showCurrency);
 right.addEventListener("click", showCommissions);
@@ -9,8 +11,11 @@ right.addEventListener("click", showCommissions);
 function showCommissions(){
     tableCurrency.style.display = "none";
     tableCommissions.style.display = "block";
+    showDate.style.backgroundColor = "#E7F5FF"
 }
 function showCurrency(){
     tableCurrency.style.display = "block";
     tableCommissions.style.display = "none";
+    
+
 }
